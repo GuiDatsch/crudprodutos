32769bb396032df2de6f3a82a4e205e35e6defcd
GUILHERME DASTCH SANTANA <771800133@fspoa.br>
2024-08-28 21:13:03 -0300

    Passo 2 - Rotiamento Basisco

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/produtos', (req, res) => {
   res.send('Listar Produtos!')
 })
+
+  app.get('/produtos/:id', (req, res) => {
+    res.send('Buscar Produtos por ID!')
+  })
+
+  app.post('/produtos', (req, res) => {
+    res.send('Inserir!')
+  })
+
+  app.put('/produtos/:id', (req, res) => {
+    res.send('Atualizar Produtos!')
+  })
+
+  app.delete('/produtos/:id', (req, res) => {
+    res.send('Deletar Produtos!')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
